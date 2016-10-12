8c8b462fa2990c48b104243ffab027c632da1b04
moulyasomasundara2016 <msomasun@stevens.edu>
2016-10-12 16:07:09 -0400

    Add files via upload

---
+++ UserStory.java
@@ -0,0 +1,37 @@
+package com;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class UserStory {
+	
+	public int NumberOfChildren(String filepath) throws FileNotFoundException{
+		int count=0;
+		File file = new File(filepath);
+		Scanner input = new Scanner(file);
+		while (input.hasNextLine()) {
+            String line = input.nextLine();
+            String[] lineArr = line.split(" ");
+            String tagName = lineArr[1];
+            if(tagName.equals("CHIL")) count++;
+        }
+		input.close();
+		return count;
+	}
+	public int NumberOfMarried(String filepath) throws FileNotFoundException{
+		int count=0;
+		File file = new File(filepath);
+		Scanner input = new Scanner(file);
+		while (input.hasNextLine()) {
+            String line = input.nextLine();
+            String[] lineArr = line.split(" ");
+            String tagName = lineArr[1];
+            if(tagName.equals("_MARNM")) count++;
+        }
+		input.close();
+		return count;
+	}
+
+}
+
